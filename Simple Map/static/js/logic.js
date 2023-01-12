ce380fc97f64df4377246457b6454e3fff14d56b
manasidek <manasidekate1410@gmail.com>
2023-01-12 16:40:36 -0500

    Adding Mapping Mutiple

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -16,15 +16,17 @@ let map = L.map("mapid").setView([40.7, -94.5], 4);
 
 //L.circleMarker([34.0522, -118.2437]).addTo(map);
 
-L.circleMarker([34.0522, -118.2437], {
-    // circleMarker.bindPopup("I am a circle.")
-    radius: 300,
-    color: "black",
-    fillColor: 'ffffa1'
+// L.circleMarker([34.0522, -118.2437], {
+//     // circleMarker.bindPopup("I am a circle.")
+//     radius: 300,
+//     color: "black",
+//     fillColor: 'ffffa1'
     
- }).addTo(map)
-   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-   .openPopup();
+//  }).addTo(map)
+//    .bindPopup("I am a circle.")
+//    .openPopup();
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -34,3 +36,19 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+  // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location,{
+        radius: city.population/200000,
+        color: "orange",
+        fillColor: "#ff7800",
+        weight: 4
+    })
+        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+        .addTo(map);
+});
