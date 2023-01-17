5b9adb57154143200058dc2beb474d0a48519cd6
manasidek <manasidekate1410@gmail.com>
2023-01-16 20:07:52 -0500

    Adding to Polygons

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -2,13 +2,13 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let Navday = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satStreet = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,38 +16,39 @@ let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-nigh
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    NavigationDay: Navday,
-    NavigationNight: night
+    "Streets": streets,
+    "Satellite Street": satStreet
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [Navday]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the toronto airline route GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/manasidek/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Simple%20Map/static/data/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/manasidek/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Simple%20Map/static/data/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-    color: "#ffffa1",
-    weight: 2
+    color: "blue",
+    fillColor: "yellow",
+    weight: 1
 }
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
         console.log(layer);
-        layer.bindPopup(`<h3>Airline Code:${feature.properties.airline}</h3><hr><h3>Destination:${feature.properties.dst}</h3>`);
+        layer.bindPopup(`<h3>Neighborhood:${feature.properties.AREA_NAME}</h3>`);
         }
      }).addTo(map);
 });
