e19200e6f0f50da92936e674412ad403dc97c324
manasidek <manasidekate1410@gmail.com>
2023-01-16 20:26:09 -0500

    Adding to Earthquakes_past7days

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -22,8 +22,8 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 })
 
@@ -31,7 +31,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the toronto airline route GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/manasidek/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Simple%20Map/static/data/torontoNeighborhoods.json";
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Create a style for the lines.
 let myStyle = {
@@ -41,14 +41,14 @@ let myStyle = {
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(earthquakeData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
         console.log(layer);
-        layer.bindPopup(`<h3>Neighborhood:${feature.properties.AREA_NAME}</h3>`);
+        layer.bindPopup(`<h3>Magnitude:${feature.properties.mag}</h3><hr><h3>Neighborhood:${feature.properties.place}</h3>`);
         }
      }).addTo(map);
 });
